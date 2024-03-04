436cc9313ed348d28d83df3f75257b0d56b59653
Eduarda Matos <duda.mdev@gmail.com>
2024-03-04 20:46:00 -0300

    Criação do Main

---
+++ primeiroProjeto4DSM/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './pages/main,js';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
